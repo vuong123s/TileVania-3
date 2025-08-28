fc861b787b2b2541539ea5f2882690502c3c1adb
rickdavidson <rickinaction@gmail.com>
2025-08-28 17:19:51 +1000

    27_TT_UC2D4 How To Create Hazards

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -88,7 +88,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Die()
     {
-        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies")))
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards")))
         {
             isAlive = false;
             myAnimator.SetTrigger("Dying");
