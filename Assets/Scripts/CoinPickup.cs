029a2b4fec92f14a615c642458f5c561e5583612
rickdavidson <rickinaction@gmail.com>
2025-09-03 10:29:47 +1000

    31_TT_UC2D4 Add A Sound Effect

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickupSFX;
+
+    bool wasCollected = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !wasCollected)
         {
+            wasCollected = true;    
+            AudioSource.PlayClipAtPoint(coinPickupSFX, transform.position);
+            gameObject.SetActive(false);
             Destroy(gameObject);
         }
     }
